07ebf96e766f9bf477b5ddd76f0038d4c1f82b5c
Robert Klock <rklock@clyde>
2018-12-05 14:58:39 -0500

    functional

--- Analyzer/analyzer.c
+++ Analyzer/analyzer.c
@@ -18,7 +18,7 @@ int getrating(char * werd){
   if (file != NULL){
   //parse through each line, grabbing the word and its respective rating 
       while(fgets(line,sizeof line,file) != NULL){
-	
+	word=strtok(line,comma);
 	sscanf(line,"%s %d",word2, &rating);
       
       for(i=0;i<2;i++){
@@ -62,65 +62,59 @@ int getrating(char * werd){
       }
     }
   }
+  fclose(file2);
     //if the word isnt found, its rating is 0
   return 0;
    
 }
 
 int generate_rating(FILE * tweet, char * n){
-  char word[144];
-  char word2[144];
-  char name[144];
+  //char word[144];
+  char tweet_word[144]; //the word grabbed from the tweet 
+  char name[144]; //the lowercase version of the name passed in 
   char punc[5] = ".,!?";
   int name_found =0;
-  int temp = global_rating;
-  int instance_rating;
-  //strcpy(name,n);
-  //name[0]=tolower(name[0]);
-  //printf("Name Lower: %s\n",name);
+  int temp;//= global_rating;
+  int instance_rating = 0;
   
-  for(int i = 0;i<strlen(n);i++){
-    if (isalpha(n[i]) && !ispunct(n[i])){
+  for(int i =0;i<strlen(n);i++){
+    if(isalpha(n[i])&&!ispunct(n[i])){
       name[i]=tolower(n[i]);
     }
   }
-  printf("NEW NAME: %s",name);
+  //name is the tolower() version of name
+  //printf("NEW NAME: %s\n",name);
   
-  while(fscanf(tweet, " %144s", word2)==1){
-   printf("word: %s\n",word2);
-   temp = getrating(word2);
-   
-   for(int i = 0; i < strlen(word2); i++){
-     if (isalpha(n[i]) && !ispunct(n[i])){
-       word[i]=tolower(n[i]);
+  while(fscanf(tweet, " %144s", tweet_word)==1){
+   temp = getrating(tweet_word);
+   instance_rating = instance_rating + temp;
+   for(int i = 0; i < strlen(tweet_word); i++){
+     if (isalpha(tweet_word[i]) && !ispunct(tweet_word[i])){
+       tweet_word[i]=tolower(tweet_word[i]);
      
      }
    }
   
-   int check = strcmp(word,name);
-   if (check==0){
+   int name_check = strcmp(tweet_word,name);
+   if (name_check==0){
      name_found=1;
-     printf("found name\n");
    }
-   instance_rating = instance_rating + temp + getrating(word);
-   printf("instance_rating : %d \n",instance_rating);
+   //instance_rating = instance_rating + temp;
+   
     
   }
 
     if(name_found==0){
-      printf("Name not found in tweet\n");
       instance_rating=0;
     }
     else{
-      printf("Name Found\n");
-      instance_rating = global_rating + instance_rating;
-    }
+      instance_rating = temp + instance_rating;
+       }
+
     
-    printf("rating for this instance, where name is %s = %d\n global rating is: %d\n",name, instance_rating, global_rating);
+    global_rating=global_rating + instance_rating;
     printf("Global Rating for %s : %d \t Instance Rating for %s : %d", name, global_rating, name, instance_rating);
-    printf("\n name_found == %d\n",name_found);
-
-
+    
     return instance_rating;
 }
 
@@ -131,9 +125,12 @@ void main(){
     
   FILE * tt = fopen("tweet.txt","r");
   r = generate_rating(tw,n);
-  printf("Favorability rating for OBAMA is: %d\n",r);
+  printf("Favorability rating for phil is: %d\n",r);
   char * nn = "Obama";
   int p = generate_rating(tt,nn);
   printf("Favorability rating for Obama is : %d\n",p);
+  FILE * tp = fopen("tweet.txt","r");
+  int f = generate_rating(tp,nn);
+  printf("Global rating: %d\n",global_rating);
   
 }
