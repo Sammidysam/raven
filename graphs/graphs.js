dda25f128e08280da7518dad990d3566e83c5a29
Sam Craig <craigsamm@gmail.com>
2018-12-15 15:57:05 -0500

    First graph: of our current James Comey data

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,265 +1,81 @@
-'use strict';
+createData = function (data) {
+	// Split our file into lines, removing any blank lines.
+	var lines = data.split("\n").filter(function (a) {
+		return a != ""
+	})
 
-window.chartColors = {
-    red: 'rgb(255, 99, 132)',
-    orange: 'rgb(255, 159, 64)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(75, 192, 192)',
-    blue: 'rgb(54, 162, 235)',
-    purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(201, 203, 207)'
-};
+	// Split each line into its attributes (separated by commas).
+	var attrs = lines.map(function (a) {
+		return a.split(",")
+	})
 
-(function(global) {
-    var MONTHS = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
-
-    var COLORS = [
-        '#4dc9f6',
-        '#f67019',
-        '#f53794',
-        '#537bc4',
-        '#acc236',
-        '#166a8f',
-        '#00a950',
-        '#58595b',
-        '#8549ba'
-    ];
-
-    var Samples = global.Samples || (global.Samples = {});
-    var Color = global.Color;
-
-    Samples.utils = {
-        // Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
-        srand: function(seed) {
-            this._seed = seed;
-        },
-
-        rand: function(min, max) {
-            var seed = this._seed;
-            min = min === undefined ? 0 : min;
-            max = max === undefined ? 1 : max;
-            this._seed = (seed * 9301 + 49297) % 233280;
-            return min + (this._seed / 233280) * (max - min);
-        },
-
-        numbers: function(config) {
-            var cfg = config || {};
-            var min = cfg.min || 0;
-            var max = cfg.max || 1;
-            var from = cfg.from || [];
-            var count = cfg.count || 8;
-            var decimals = cfg.decimals || 8;
-            var continuity = cfg.continuity || 1;
-            var dfactor = Math.pow(10, decimals) || 0;
-            var data = [];
-            var i, value;
-
-            for (i = 0; i < count; ++i) {
-                value = (from[i] || 0) + this.rand(min, max);
-                if (this.rand() <= continuity) {
-                    data.push(Math.round(dfactor * value) / dfactor);
-                } else {
-                    data.push(null);
-                }
-            }
-
-            return data;
-        },
-
-        labels: function(config) {
-            var cfg = config || {};
-            var min = cfg.min || 0;
-            var max = cfg.max || 100;
-            var count = cfg.count || 8;
-            var step = (max - min) / count;
-            var decimals = cfg.decimals || 8;
-            var dfactor = Math.pow(10, decimals) || 0;
-            var prefix = cfg.prefix || '';
-            var values = [];
-            var i;
-
-            for (i = min; i < max; i += step) {
-                values.push(prefix + Math.round(dfactor * i) / dfactor);
-            }
-
-            return values;
-        },
-
-        months: function(config) {
-            var cfg = config || {};
-            var count = cfg.count || 12;
-            var section = cfg.section;
-            var values = [];
-            var i, value;
-
-            for (i = 0; i < count; ++i) {
-                value = MONTHS[Math.ceil(i) % 12];
-                values.push(value.substring(0, section));
-            }
-
-            return values;
-        },
-
-        color: function(index) {
-            return COLORS[index % COLORS.length];
-        },
-
-        transparentize: function(color, opacity) {
-            var alpha = opacity === undefined ? 0.5 : 1 - opacity;
-            return Color(color).alpha(alpha).rgbString();
-        }
-    };
-
-    // DEPRECATED
-    window.randomScalingFactor = function() {
-        return Math.round(Samples.utils.rand(-100, 100));
-    };
-
-    // INITIALIZATION
-
-    Samples.utils.srand(Date.now());
-
-    // Google Analytics
-    /* eslint-disable */
-    if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-28909194-3', 'auto');
-        ga('send', 'pageview');
-    }
-    /* eslint-enable */
-
-}(this));
-
-var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var config = {
-	type: 'line',
-	data: {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-		datasets: [{
-			label: 'My First dataset',
-			backgroundColor: window.chartColors.red,
-			borderColor: window.chartColors.red,
-			data: [
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor()
-			],
-			fill: false,
-		}, {
-			label: 'My Second dataset',
-			fill: false,
-			backgroundColor: window.chartColors.blue,
-			borderColor: window.chartColors.blue,
-			data: [
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor(),
-				randomScalingFactor()
-			],
-		}]
-	},
-	options: {
-		responsive: true,
-		title: {
-			display: true,
-			text: 'Chart.js Line Chart'
-		},
-		tooltips: {
-			mode: 'index',
-			intersect: false,
-		},
-		hover: {
-			mode: 'nearest',
-			intersect: true
-		},
-		scales: {
-			xAxes: [{
-				display: true,
-				scaleLabel: {
-					display: true,
-					labelString: 'Month'
-				}
-			}],
-			yAxes: [{
-				display: true,
-				scaleLabel: {
-					display: true,
-					labelString: 'Value'
-				}
-			}]
+	// Turn each attrs into an object.
+	// Basically, we are assigning a variable name to each
+	// comma-separated value.
+	var objs = attrs.map(function (a) {
+		return {
+			analysis_date: a[0],
+			id: a[1],
+			rating: parseInt(a[2]),
+			tweet_date: a[3],
+			ref_link: a[4]
 		}
+	})
+
+	// Sort the objs by their tweet date.
+	objs.sort(function (a, b) {
+		// Turn each string into a date.
+		ad = new Date(a.tweet_date)
+		bd = new Date(b.tweet_date)
+
+		return ad < bd ? ((bd == ad) ? 0 : -1) : 1
+	})
+
+	// Fill in our two main data series:
+	// the tweet date of the tweet
+	// and the total analysis score
+	var total = 0
+	var return_data = {
+		tweet_dates: [],
+		ratings: []
 	}
+
+    $.each(objs, function() {
+		total += this.rating
+
+        return_data.tweet_dates.push(this.tweet_date)
+		return_data.ratings.push(total)
+    })
+
+	return return_data
 };
-window.onload = function() {
-	var ctx = document.getElementById('canvas').getContext('2d');
-	window.myLine = new Chart(ctx, config);
-};
-document.getElementById('randomizeData').addEventListener('click', function() {
-	config.data.datasets.forEach(function(dataset) {
-		dataset.data = dataset.data.map(function() {
-			return randomScalingFactor();
-		});
-	});
-	window.myLine.update();
-});
-var colorNames = Object.keys(window.chartColors);
-document.getElementById('addDataset').addEventListener('click', function() {
-	var colorName = colorNames[config.data.datasets.length % colorNames.length];
-	var newColor = window.chartColors[colorName];
-	var newDataset = {
-		label: 'Dataset ' + config.data.datasets.length,
-		backgroundColor: newColor,
-		borderColor: newColor,
-		data: [],
-		fill: false
-	};
-	for (var index = 0; index < config.data.labels.length; ++index) {
-		newDataset.data.push(randomScalingFactor());
-	}
-	config.data.datasets.push(newDataset);
-	window.myLine.update();
-});
-document.getElementById('addData').addEventListener('click', function() {
-	if (config.data.datasets.length > 0) {
-		var month = MONTHS[config.data.labels.length % MONTHS.length];
-		config.data.labels.push(month);
-		config.data.datasets.forEach(function(dataset) {
-			dataset.data.push(randomScalingFactor());
-		});
-		window.myLine.update();
-	}
-});
-document.getElementById('removeDataset').addEventListener('click', function() {
-	config.data.datasets.splice(0, 1);
-	window.myLine.update();
-});
-document.getElementById('removeData').addEventListener('click', function() {
-	config.data.labels.splice(-1, 1); // remove the label first
-	config.data.datasets.forEach(function(dataset) {
-		dataset.data.pop();
-	});
-	window.myLine.update();
-});
+
+createGraph = function (data) {
+	// Create the chart.
+    var ctx = document.getElementById("canvas").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.tweet_dates,
+            datasets: [{
+                label: 'Favorability Rating',
+                data: data.ratings,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    })
+}
+
+// Retrieve our data and process it.
+$.get("/James%20Comey.out", function (data) {
+	createGraph(createData(data))
+})
