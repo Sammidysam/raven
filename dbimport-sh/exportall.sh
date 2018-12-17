f9ad6b778e762246181fac2bbab8a2c53f73dff2
Sam Craig <craigsamm@gmail.com>
2018-12-17 18:26:51 -0500

    Export as a JSON array
    This will be important for reanalyzing

--- dbimport-sh/exportall.sh
+++ dbimport-sh/exportall.sh
@@ -5,5 +5,5 @@ COLLECTIONS=$(mongo localhost:27017/$DB --quiet --eval "db.getCollectionNames()"
 
 for collection in $COLLECTIONS; do
     echo "Exporting $DB/$collection ..."
-    mongoexport -d $DB -c $collection -o $collection.json
+    mongoexport -d $DB -c $collection -o $collection.json --jsonArray
 done
