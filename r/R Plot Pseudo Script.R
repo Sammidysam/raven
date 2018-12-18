e51223662a94c327de664c91a749b0d645f7d461
RobKlock <rklock@oberlin.edu>
2018-12-18 00:06:08 -0500

    Update R Plot Pseudo Script.R

--- r/R Plot Pseudo Script.R
+++ r/R Plot Pseudo Script.R
@@ -2,5 +2,5 @@ FILE = <path to file>
 x_data <- read.csv(FILE,header=FALSE)
 vars = c("V3","V1")
 sub_data=x_data[vars]
-plot(sub_data,xlab="Rating",ylab="Creation Time",Title = "")
+plot(sub_data,xlab="Rating",ylab="Creation Time",main= "")
 
